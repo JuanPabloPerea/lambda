3f1f1d8c528d0c7d59df45f883c01880a410dcc5
JuanPabloPerea <36460406+JuanPabloPerea@users.noreply.github.com>
2020-06-25 12:36:37 -0500

    Update lambda recursivo.py

--- lambda recursivo.py
+++ lambda recursivo.py
@@ -1,12 +1,10 @@
-def recursive(def_fun):
-    def wrapper(p,**kw):
-        fi = lambda p, **kw: def_fun(fi, p, **kw)
-        return def_fun(fi, p, **kw)
+def recursivo(funtion):
+    def envoltura(num1,**num2):
+        fi = lambda num1, **num2: funtion(fi, num1, **num2)
+        return def_fun(fi, num1, **num2)
 
     return wrapper
 
 fibonaci = recursive(lambda f, n: f(n - 1) + f(n - 2) if n > 1 else 1)
 print(fibonaci(10))
 
-factorial = recursive(lambda f, n: 1 if n < 2 else n * f(n - 1))
-print(factorial(10))
